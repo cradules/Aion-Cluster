f0954576dc4aaec059d0639b0c7dda16a0a412d1
Ubuntu <cradulescu@crypto-vm01-prod-we-as.j4fzchqcinpe5kjhmnbrhaarag.ax.internal.cloudapp.net>
2018-04-22 11:27:18 +0000

    Add cd to basedir

--- main.sh
+++ main.sh
@@ -8,9 +8,9 @@
 #Create log
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/var/log/cluster.log 2>&1
+exec 1>>/var/log/cluster.log 2>&1
 
-
+cd "$(dirname "$0")"
 . ./cluster.cfg
 
 #Check if brother is alive
@@ -18,27 +18,28 @@ timeout 2 bash -c "</dev/tcp/$BROTHER/3333" >/dev/null 2>&1
 RC=$(echo $?)
 if [ $RC -eq 0 ]
 	then
-        echo "Brother $BROTHER is up"
+        echo "$DATE Brother $BROTHER is up"
+	exit 0
 else
-	echo "Brother $BROTHER down"
-	echo "Checking services local"
+	echo "$DATE Brother $BROTHER is down"
+	echo "$DATE Checking services if running locally"
 	
 	#Check if the services are runing on current server
 	timeout 2 bash -c "</dev/tcp/$HOSTNAME/3333" >/dev/null 2>&1
 	RC=$(echo $?)
 	if [[ $RC -eq 0 ]]
         	then
-        	echo "I am alive"
+        	echo "$DATE I am alive"
 		#Check redis server
-        	echo "Checking redis server"
+        	echo "$DATE Checking redis server"
         	timeout 2 bash -c "</dev/tcp/$HOSTNAME/6379" >/dev/null 2>&1
         	RC=$(echo $?)
 			if [[ $RC -eq 0 ]]
                 		then
-                		echo "Redis Server is UP"
+                		echo "$DATE Redis Server is UP"
                 		exit 0
         		else
-                		echo "Redis Server is DOWN"
+                		echo "$DATE Redis Server is DOWN"
       			fi
 
 	else
@@ -60,7 +61,7 @@ else
 				lvscan
 			fi
 			mount /dev/vgcentryspool/lvcentryspool /pool-app
-			echo "File systems is mounted"
+			echo "$DATE File systems is mounted"
 		fi
 				
 		#Check if node is up
@@ -68,20 +69,21 @@ else
 		RC=$(echo $?)
 		if [[ $RC -eq 0 ]]
 			then
-			echo "Node is up"
+			echo "$DATE Node is up"
 		else
-			#Stop backround proccess
+			#Stop backroound proccess
+			echo "$DATE Node is down..."
 			for y in `ps -ef | grep "./aion.sh" | grep -v "grep" | awk '{print $2}'`; do sudo kill -9 $y; done
 			for y in `netstat -tenpl | grep -e 8547 -e 8545 -e 8080| awk '{print $9}' | awk -F '/' '{print $1}'`; do kill -9 $y; done
 			#Start node services	
-			echo "Starting node"
+			echo "$DATE Starting node"
 			cd /pool-app/node
 			su -c "./aion.sh" crypto > /dev/null 2>&1 &
 		fi
 		#Clean up backround proccess
 		for y in `netstat -tenpl | grep -v  -e 8547 -e 8545 -e 8080 | grep -e 3333 -e 6379| awk '{print $9}' | awk -F '/' '{print $1}'`; do kill -9 $y; done
-		echo "Aion pool is down"
-        	echo "Starting aion-pool"
+		echo "$DATE Aion pool is down"
+        	echo "$DATE Starting aion-pool"
         	cd /pool-app/aion-pool
        		su -c "./run.sh" crypto  > /dev/null 2>&1 &
 	fi
