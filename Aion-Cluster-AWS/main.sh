ac0b1b0a910dcf59a0275cc18e7411334f42b23e
Constantin Radulescu <constantin.radulescu@centrys.io>
2018-05-04 17:06:23 +0000

    Initial Commit

---
+++ Aion-Cluster-AWS/main.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#set -x
+##############################################################################
+#Main script will verify if "brother is awake" and will take the decision if 
+# stays or goes to sleep!
+##############################################################################
+
+
+. ./cluster.cfg
+
+#Check if brother is alive
+timeout 2 bash -c "</dev/tcp/$BROTHER/3333" >/dev/null 2>&1
+RC=$(echo $?)
+if [ $RC -eq 0 ]
+	then
+        echo "$DATE Brother $BROTHER is up" >> $LOGFILE
+	exit 0
+else
+	echo "$DATE Brother $BROTHER is down" >> $LOGFILE
+	echo "$DATE Checking services if running locally" >> $LOGFILE
+	
+	#Check if the services are runing on current server
+	timeout 2 bash -c "</dev/tcp/$HOSTNAME/3333" >/dev/null 2>&1
+	RC=$(echo $?)
+	if [[ $RC -eq 0 ]]
+        	then
+        	echo "$DATE I am alive" >> $LOGFILE
+		#Check redis server
+        	echo "$DATE Checking redis server" >> $LOGFILE
+        	timeout 2 bash -c "</dev/tcp/$HOSTNAME/6379" >/dev/null 2>&1
+        	RC=$(echo $?)
+			if [[ $RC -eq 0 ]]
+                		then
+                		echo "$DATE Redis Server is UP" >> $LOGFILE
+                		exit 0
+        		else
+                		echo "$DATE Redis Server is DOWN" >> $LOGFILE
+      			fi
+
+	else
+		#Check if filesystem is mounted
+		ISMOUNT=$(grep -c -e pool-app -e nfs /etc/mtab)
+		if [[ $ISMOUNT -ne 1 ]]
+			then
+			#Attach and mount disk to me
+			echo "$DATE Mounting $SHAREDDISK" >> $LOGFILE
+			/usr/local/bin/aionmount start	
+		else
+			echo "$DATE Cant mount pool-app. Most probrablly the disk could not be attached" >> $LOGFILE
+		fi
+				
+		#Check if node is up
+		timeout 2 bash -c "</dev/tcp/$HOSTNAME/8545" >/dev/null 2>&1
+		RC=$(echo $?)
+		if [[ $RC -eq 0 ]]
+			then
+			echo "$DATE Node is up"  >> $LOGFILE
+		else
+			#Restart node
+			 echo "$DATE Starting node" >> $LOGFILE
+			systemctl restart  nodepool
+		fi
+		#Restart Aion Pool
+		echo "$DATE Starting node"  >> $LOGFILE
+		systemctl restart aionpool
+		
+	fi
+exit 0
+
+fi
